cffe03ce75e1b256c71c7824a4760764d0bc676a
ja123mano <josesantana99@hotmail.com>
2020-08-26 22:00:20 -0500

    Para la tarea

--- labs/01/hello.c
+++ labs/01/hello.c
@@ -4,6 +4,6 @@
 
 int main(void) {
     printf("Hello World\n");
-    printf("My name is Christopher Salvador Márquez Álvarez \n");
-    return 0; 
+    printf("My name is Jose Enrique Santana Martinez \n");
+	    return 0; 
 }
